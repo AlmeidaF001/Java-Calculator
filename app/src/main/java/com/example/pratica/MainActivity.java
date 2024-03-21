2af57a70f16593ee47824990ac03669a6aa6801d
RL <rubenfilipe.almeida@my.istec.pt>
2024-03-21 19:16:38 +0000

    Versão Final

--- app/src/main/java/com/example/pratica/MainActivity.java
+++ app/src/main/java/com/example/pratica/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,17 +13,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-
-//Método onCreate é chamado quando a atividade é criada. Ele realiza várias inicializações,
-// incluindo a configuração da interface do utilizador, e a definição da ActionBar (usando a Toolbar)
-// e a adição do fragmento Home à atividade.
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageView imageView = findViewById(R.id.img_home_screen);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar_toolbar_main);
@@ -47,9 +40,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-
-    //Método onOptionsItemSelected é chamado quando um item do menu é selecionado. Ele verifica o ID do item selecionado e exibe um Toast com uma mensagem correspondente.
-    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.mnu_calculadora_mainmenu) {
@@ -63,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.mnu_selos_mainmenu) {
             Toast.makeText(this, "Selos", Toast.LENGTH_SHORT).show();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            Fragment selos = Selos.newInstance("","");
+            Fragment selos = Selos.newInstance("", "");
             ft.replace(R.id.fragmentContainerView, selos);
             ft.addToBackStack(null);
             ft.commit();
--- app/src/main/java/com/example/pratica/Selos.java
+++ app/src/main/java/com/example/pratica/Selos.java
@@ -1,43 +1,56 @@
 package com.example.pratica;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import java.util.ArrayList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.app.Activity;
 
-public class Selos extends Fragment{
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-   public Integer scinco;
-   public Integer stres;
+public class Selos extends Fragment {
 
-   TextView TROCO;
-   
+    public Integer scinco;
+    public Integer stres;
+
+    TextView TROCO;
+    EditText valorDinheiroEditText;
+    EditText selosConvertidosEditText;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         this.scinco = 0;
         this.stres = 0;
-        TROCO = view.findViewById(R.id.txt_troco);
-        this.trocos(12);
-        TROCO.setText("CINCO:"+this.scinco.toString()+"   "+"TRES:"+this.stres.toString());
+        TROCO = view.findViewById(R.id.converter_selos_troco); // Corrigido para pegar a referência correta do TextView
+        valorDinheiroEditText = view.findViewById(R.id.converter_dinheiro_selos);
+        selosConvertidosEditText = view.findViewById(R.id.converter_selos_covertido);
+        Button converterButton = view.findViewById(R.id.button_converter_selos);
+
+        converterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String valorDinheiroStr = valorDinheiroEditText.getText().toString();
+                if (!valorDinheiroStr.isEmpty()) {
+                    int valorDinheiro = Integer.parseInt(valorDinheiroStr);
+                    trocos(valorDinheiro);
+                    TROCO.setText("Selos de 5 :" + scinco.toString() + "   " + "Selos de 3 :" + stres.toString());
+                    selosConvertidosEditText.setText(scinco.toString()); // Definindo o valor dos selos convertidos no EditText
+                }
+            }
+        });
     }
-    
-    public Selos(){
-        
+
+
+    public Selos() {
+
     }
-    
-    
-        // TODO: Rename parameter arguments, choose names that match
+
+    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -45,9 +58,8 @@ public class Selos extends Fragment{
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    
-    
-    
+
+
     public static Selos newInstance(String param1, String param2) {
         Selos fragment = new Selos();
         Bundle args = new Bundle();
@@ -56,7 +68,6 @@ public class Selos extends Fragment{
         fragment.setArguments(args);
         return fragment;
     }
-
 
 
     @Override
@@ -76,56 +87,54 @@ public class Selos extends Fragment{
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_selos, container, false);
     }
-    
-    
-    
-    
-    public void trocos(int euros){
-        if(euros >= 8){
-	        Integer Quoc = euros/8;
-    	        int R = euros % 8;
-	        switch(R){
-		        case 0:
-			        this.scinco = Quoc;
-			        this.stres = Quoc;
-			        break;
-		        case 1:
-			        this.scinco = Quoc - 1;
-    			        this.stres = Quoc + 2;
-    			        break;
-		        case 2:
-			        this.scinco = Quoc + 1;
-			        this.stres = Quoc - 1;
-			        break;
-		        case 3:
-			        this.scinco = Quoc;
-			        this.stres = Quoc + 1;
-			        break;
-		        case 4:
-			        this.scinco = Quoc - 1;
-			        this.stres = Quoc + 3;
-			        break;
-		        case 5:
-			        this.scinco = Quoc + 1;
-			        this.stres = Quoc;
-			        break;
-		        case 6:
-			        this.scinco = Quoc;
-			        this.stres = Quoc + 2;
-			        break;
-		        case 7:
-			        this.scinco = Quoc + 2;
-			        this.stres = Quoc - 1;
-			        break;
+
+
+    public void trocos(int euros) {
+        if (euros >= 8) {
+            int Quoc = euros / 8;
+            int R = euros % 8;
+            switch (R) {
+                case 0:
+                    this.scinco = Quoc;
+                    this.stres = Quoc;
+                    break;
+                case 1:
+                    this.scinco = Quoc;
+                    this.stres = Quoc + 1;
+                    break;
+                case 2:
+                    this.scinco = Quoc;
+                    this.stres = Quoc + 2;
+                    break;
+                case 3:
+                    this.scinco = Quoc;
+                    this.stres = Quoc + 1;
+                    break;
+                case 4:
+                    this.scinco = Quoc;
+                    this.stres = Quoc + 2;
+                    break;
+                case 5:
+                    this.scinco = Quoc + 1;
+                    this.stres = Quoc;
+                    break;
+                case 6:
+                    this.scinco = Quoc;
+                    this.stres = Quoc + 1;
+                    break;
+                case 7:
+                    this.scinco = Quoc + 1;
+                    this.stres = Quoc;
+                    break;
             }
         } else {
-	        if(euros == 3 || euros == 5){
-		        this.stres = 1;
-	        } else if( euros == 6){
-		        this.stres = 2;
-	        } else {
-		        System.out.println("devolver euros");
-	        }
+            if (euros == 3 || euros == 5) {
+                this.stres = 1;
+            } else if (euros == 6) {
+                this.stres = 2;
+            } else {
+                System.out.println("devolver euros");
+            }
         }
     }
 }
